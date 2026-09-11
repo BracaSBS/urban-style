5505bb4680e421a7cf61235601be1f4c75f906cc
Santiago Bracamonte <santiagobracamonte07@gmail.com>
2026-09-11 08:16:04 -0500

    Crear estructura inicial de base de datos

---
+++ 04-Base-de-datos/urban_style.sql
@@ -0,0 +1,12 @@
+-- =========================================================
+-- URBAN STYLE
+-- Sistema de Gestión de Inventario y Ventas
+-- =========================================================
+
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS urban_style
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Seleccionar la base de datos
+USE urban_style;
