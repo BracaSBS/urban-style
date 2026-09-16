bdc390b3e3347738c22697185029643aaaec901e
Santiago Bracamonte <santiagobracamonte07@gmail.com>
2026-09-16 08:23:13 -0500

    Integra backend Spring Boot

---
+++ 05-Desarrollo/backend/src/main/java/com/urbanstyle/urbanstyle/UrbanStyleApplication.java
@@ -0,0 +1,13 @@
+package com.urbanstyle.urbanstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanStyleApplication.class, args);
+	}
+
+}
---
+++ 05-Desarrollo/backend/src/test/java/com/urbanstyle/urbanstyle/UrbanStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanstyle.urbanstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
